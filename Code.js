a5a1613b7aec22584184b3053b6ea03b09f99a94
Evgeniy Khoroshilov <33131176+keitmn@users.noreply.github.com>
2022-01-27 20:28:50 +0300

    Update Code.js

--- Code.js
+++ Code.js
@@ -2,7 +2,7 @@ function doPost(request) {
   
   const {userID, sum, category} = request.parameter
   const sheet = SpreadsheetApp.getActiveSheet()
-  const maxRows = sheet.getMaxRows()
+  const lastRow = sheet.getLastRow() + 1
 
   if (!userID) {
     return ContentService.createTextOutput(JSON.stringify({success: false, error: `Не указан ID пользователя`})).setMimeType(ContentService.MimeType.JSON)
@@ -11,15 +11,10 @@ function doPost(request) {
   } else if (!category) {
     return ContentService.createTextOutput(JSON.stringify({success: false, error: `Не указана категория расходов`})).setMimeType(ContentService.MimeType.JSON)
   }
-
-  for(let i = 2; i < maxRows; i++) {
-    if (!sheet.getRange(`A${i}`).getValue()) {
-      sheet.getRange(`A${i}`).setValue(userID)
-      sheet.getRange(`B${i}`).setValue(sum)
-      sheet.getRange(`C${i}`).setValue(category)
-      break
-    }
-  }
+  
+  sheet.getRange(`A${lastRow}`).setValue(userID)
+  sheet.getRange(`B${lastRow}`).setValue(sum)
+  sheet.getRange(`C${lastRow}`).setValue(category)
   
   const result = getResult(request.parameter)
   return ContentService.createTextOutput(JSON.stringify(result)).setMimeType(ContentService.MimeType.JSON)
@@ -37,10 +32,10 @@ function doGet(request) {
 
 function getResult({userID, category}) {
   const sheet = SpreadsheetApp.getActiveSheet()
-  const maxRows = sheet.getMaxRows()
+  const lastRow = sheet.getLastRow() + 1
   const result = {userID, category, sum: 0, success: true}
 
-  for(let i = 2; i < maxRows; i++) {
+  for(let i = 2; i < lastRow; i++) {
     let id = sheet.getRange(`A${i}`).getValue()
     let currCategory = sheet.getRange(`C${i}`).getValue()
 
